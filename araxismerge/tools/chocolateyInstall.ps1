c8f9f83297a15262c47e1dd4bd1a33dad66565a6
Robin Minto <robin@minto.co.uk>
2016-12-02 13:02:42 +0000

    Araxis Merge 2016.4801

--- araxismerge/tools/chocolateyInstall.ps1
+++ araxismerge/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'araxismerge'
 $installerType = 'MSI'
-$url = 'http://www.araxis.com/download/Merge2016.4761-Win32.msi'
-$url64 = 'http://www.araxis.com/download/Merge2016.4761-x64.msi'
+$url = 'https://www.araxis.com/download/Merge2016.4801-Win32.msi'
+$url64 = 'https://www.araxis.com/download/Merge2016.4801-x64.msi'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
