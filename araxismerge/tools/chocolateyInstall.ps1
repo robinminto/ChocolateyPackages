48b6dc2d8094ce61cacd1d789c70e0d7977cf654
Robin Minto <robin@minto.co.uk>
2016-12-12 14:12:42 +0000

    Added checksums

--- araxismerge/tools/chocolateyInstall.ps1
+++ araxismerge/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $packageName = 'araxismerge'
 $installerType = 'MSI'
 $url32 = 'https://www.araxis.com/download/Merge2016.4801-Win32.msi'
 $url64 = 'https://www.araxis.com/download/Merge2016.4801-x64.msi'
-$checksum32  = ''
-$checksum64  = ''
+$checksum32  = '841b9f47381b3af62c748335e6a2093d711912edbf8c5fa0b31f3d9ce5723dc7'
+$checksum64  = '7052f66054dd1e1f0d257905ec3f99cf055d774640c9695c9368410fb470adaa'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
