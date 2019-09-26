54d0409185ea4b4c2ef30762702e3f0abbe39a7d
Robin Minto <robin@minto.co.uk>
2019-09-26 20:18:55 +0100

    Araxis Merge 2019.5174 with correct URLs

--- araxismerge/tools/chocolateyInstall.ps1
+++ araxismerge/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'araxismerge'
 $installerType = 'MSI'
-$url32 = 'https://www.araxis.com/download/Merge2019.5174-x64.msi'
-$url64 = 'https://www.araxis.com/download/Merge2019.5187-x64.msi'
-$checksum32  = 'fad54984ba82582eb0c91425f0d3d26f1a03aa2413ffd493ff5958e33007a479'
-$checksum64  = '87f753d8d7a1991fb0290ba55ebef8ade3907f03eace4809d5da5ed9ffe836bf'
+$url32 = 'https://www.araxis.com/download/Merge2018.5059-Win32.msi'
+$url64 = 'https://www.araxis.com/download/Merge2019.5174-x64.msi'
+$checksum32  = '5c220833b218a7f5355d724bf49deb93ba6168049362c6423901568ce8074e48'
+$checksum64  = 'fad54984ba82582eb0c91425f0d3d26f1a03aa2413ffd493ff5958e33007a479'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
