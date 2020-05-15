fea0ee056a6544dc44d934487353fbb142597020
Robin Minto <robin@minto.co.uk>
2020-05-15 14:49:04 +0100

    Only update 64-but version and use manual checksum

--- araxismerge/update.ps1
+++ araxismerge/update.ps1
@@ -6,11 +6,13 @@ function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
      }
+}
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
 }
 
 function global:au_GetLatest {
@@ -18,14 +20,10 @@ function global:au_GetLatest {
 
     $re64  = "https://www\.araxis\.com/download/Merge(?<version>\d{4}\.\d{4,})-(x64)\.msi"
     $url64 = $downloadPage.links | ? href -match $re64 | select -First 1 -expand href
-    # Use the version of the latest x64 package. Win32 version is no longer updated.
     $version = $matches.version
 
-    $re32  = "https://www\.araxis\.com/download/Merge(?<version>\d{4}\.\d{4,})-(Win32)\.msi"
-    $url32 = $downloadPage.links | ? href -match $re32 | select -First 1 -expand href
-    
-    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    $Latest = @{ URL64 = $url64; Version = $version }
     return $Latest
 }
 
-update-package -ChecksumFor all
+update-package -ChecksumFor None
