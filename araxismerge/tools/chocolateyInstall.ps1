01bcad1750016e65a16a522e9558604d193de1fc
Robin Minto <robin@minto.co.uk>
2020-08-10 11:54:12 +0100

    Araxis Merge 2020.5406

--- araxismerge/tools/chocolateyInstall.ps1
+++ araxismerge/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'araxismerge'
 $installerType = 'MSI'
 $url32 = 'https://www.araxis.com/download/Merge2018.5059-Win32.msi'
-$url64 = 'https://www.araxis.com/download/Merge2020.5368-x64.msi'
+$url64 = 'https://www.araxis.com/download/Merge2020.5406-x64.msi'
 $checksum32  = '5c220833b218a7f5355d724bf49deb93ba6168049362c6423901568ce8074e48'
-$checksum64  = 'f698abefeb46df4aadea23d298df77f3f06b6ee7a996ca0c48332fda1b633ffc'
+$checksum64  = '75cfc63bcc2f49f367a750276ff3b4b57807291f82c4dcb0a8db94f9319d692c'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
